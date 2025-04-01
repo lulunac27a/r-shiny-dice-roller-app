a191bd2d833770a262f2ffb2087fa6d06e48e6a7
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-03-31 22:42:45 -0500

    Format code

--- app.R
+++ app.R
@@ -1,21 +1,12 @@
 library(shiny)
-ui <- fluidPage(
-    titlePanel("Dice Roller App"),
-    sidebarLayout(
-        sidebarPanel(
-            numericInput("dice", "Number of Dice", value = 1, min = 1),
-            actionButton("roll", "Roll Dice")
-        ),
-        mainPanel(
-            textOutput("output")
-        )
-    )
-)
+ui <- fluidPage(titlePanel("Dice Roller App"), sidebarLayout(sidebarPanel(numericInput("dice",
+  "Number of Dice", value = 1, min = 1), actionButton("roll",
+  "Roll Dice")), mainPanel(textOutput("output"))))
 server <- function(input, output) {
-    observeEvent(input$roll, {
-        diceRolls = sample(1:6, input$dice, replace = TRUE)
-        output$output <- renderText(paste("Total dice: ", sum(diceRolls)))
-    })
+  observeEvent(input$roll, {
+    diceRolls = sample(1:6, input$dice, replace = TRUE)
+    output$output <- renderText(paste("Total dice: ", sum(diceRolls)))
+  })
 }
 
 shinyApp(ui = ui, server = server)
