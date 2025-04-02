b923fefc1713406b042b8cef4890e7bfc9dad544
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-04-02 13:59:34 -0500

    Add input for number of sides

--- app.R
+++ app.R
@@ -4,17 +4,18 @@ library(shiny)
 ui <- fluidPage(titlePanel("Dice Roller App"), sidebarLayout(sidebarPanel(numericInput("dice",
   "Number of Dice", value = 1, min = 1, step = 1)  #number of dice input
 ,
-  actionButton("roll", "Roll Dice")  #button to roll dice
-),
-  mainPanel(textOutput("diceRollSum")  #dice roll sum text output
-,
-    textOutput("diceRollValues")  #dice roll values text output
+  numericInput("sides", "Number of Sides", value = 6, min = 1,
+    step = 1)  #number of sides input
+, actionButton("roll",
+    "Roll Dice")  #button to roll dice
+), mainPanel(textOutput("diceRollSum")  #dice roll sum text output
+, textOutput("diceRollValues")  #dice roll values text output
 )))
 server <- function(input, output) {
   # when roll dice button is pressed
   observeEvent(input$roll, {
     # roll dice
-    diceRolls = sample(1:6, input$dice, replace = TRUE)
+    diceRolls = sample(1:input$sides, input$dice, replace = TRUE)
     # set output to sum of dice rolls
     output$diceRollSum <- renderText(paste("Total Dice: ",
       sum(diceRolls)))
