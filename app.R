1d87161fda919dad1bbc3b0b53ac1ab08bee0e12
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-04-01 19:10:04 -0500

    Add comments

--- app.R
+++ app.R
@@ -1,12 +1,21 @@
+# import Shiny library
 library(shiny)
+# define UI component
 ui <- fluidPage(titlePanel("Dice Roller App"), sidebarLayout(sidebarPanel(numericInput("dice",
-  "Number of Dice", value = 1, min = 1), actionButton("roll",
-  "Roll Dice")), mainPanel(textOutput("output"))))
+  "Number of Dice", value = 1, min = 1)  #number of dice input
+,
+  actionButton("roll", "Roll Dice")  #button to roll dice
+),
+  mainPanel(textOutput("output")  #text output
+)))
 server <- function(input, output) {
+  # when roll dice button is pressed
   observeEvent(input$roll, {
+    # roll dice
     diceRolls = sample(1:6, input$dice, replace = TRUE)
+    # set output to sum of dice rolls
     output$output <- renderText(paste("Total dice: ", sum(diceRolls)))
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)  #run the Shiny web app server
