738b69b9b63aa780d3689df88060f8cde228e4ba
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-04-02 20:43:33 -0500

    Add checkbox to show list of dice rolls

--- app.R
+++ app.R
@@ -6,31 +6,52 @@ ui <- fluidPage(titlePanel("Dice Roller App"), sidebarLayout(sidebarPanel(numeri
 ,
   numericInput("sides", "Number of Sides", value = 6, min = 1,
     step = 1)  #number of sides input
-, actionButton("roll",
-    "Roll Dice")  #button to roll dice
-), mainPanel(textOutput("diceRollSum")  #dice roll sum text output
-, textOutput("diceRollValues")  #dice roll values text output
-, textOutput("sortedDiceRollValues")  #sorted dice roll values text output
+, checkboxInput("rollList",
+    "Show List of Dice Rolls", value = FALSE)  #checkbox to show list of dice rolls
+,
+  actionButton("roll", "Roll Dice")  #button to roll dice
+),
+  mainPanel(textOutput("diceRollSum")  #dice roll sum text output
+,
+    textOutput("diceRollValues")  #dice roll values text output
+,
+    textOutput("sortedDiceRollValues")  #sorted dice roll values text output
+,
+    textOutput("diceRollList")  #dice roll list text output
 )))
 server <- function(input, output) {
   # when roll dice button is pressed
   observeEvent(input$roll, {
     # roll dice
     diceRolls <- sample(1:input$sides, input$dice, replace = TRUE)
+    # table of dice rolls
+    diceRollsTable <- table(diceRolls)
+    # convert table to data frame
+    sortedDiceRollsTable <- as.data.frame(diceRollsTable)
     # sort dice rolls
-    sortedDiceRolls <- diceRolls[order(diceRolls, decreasing = TRUE)]
+    sortedDiceRolls <- sortedDiceRollsTable[order(sortedDiceRollsTable$Freq,
+      decreasing = TRUE), ]
     # set output to sum of dice rolls
     output$diceRollSum <- renderText(paste("Total Dice: ",
       sum(diceRolls)))
     # set output to values of dice rolls
     output$diceRollValues <- renderText(paste("Dice Rolls by Number: \n",
-      paste(names(table(diceRolls)), table(diceRolls),
-        sep = ": ", collapse = "\n")))
+      paste(names(diceRollsTable), diceRollsTable, sep = ": ",
+        collapse = "\n")))
     # set output to sorted values of dice rolls
     output$sortedDiceRollValues <- renderText(paste("Sorted Dice Rolls by Number: \n",
-      paste(names(table(sortedDiceRolls)), table(sortedDiceRolls),
+      paste(sortedDiceRolls$diceRolls, sortedDiceRolls$Freq,
         sep = ": ", collapse = "\n")))
+    # show list of dice rolls if checkbox is checked
+    if (input$rollList) {
+      output$diceRollList <- renderText(paste("List of Dice Rolls: \n",
+        paste(diceRolls, collapse = ", ")))
+    } else {
+      output$diceRollList <- renderText("")
+    }
   })
 }
 
 shinyApp(ui = ui, server = server)  #run the Shiny web app server
+
+
